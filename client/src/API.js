c0738c98abe98540f251ac94e8342e88ecd6e7cc
cormobro <bonaet.felix@gmail.com>
2018-11-14 14:23:17 +0100

    ajout carte et composants

--- client/src/API.js
+++ client/src/API.js
@@ -1,23 +1,4 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000/api/v1/messages' : 'https://api.guestm.app/api/v1/messages';
 
-export function getMessages() {
-  return fetch(API_URL)
-    .then(res => res.json())
-    .then(messages => {
-      const haveSeenLocation = {};
-      return messages.reduce((all, message) => {
-        const key = `${message.latitude.toFixed(3)}${message.longitude.toFixed(3)}`;
-        if (haveSeenLocation[key]) {
-          haveSeenLocation[key].otherMessages = haveSeenLocation[key].otherMessages || [];
-          haveSeenLocation[key].otherMessages.push(message);
-        } else {
-          haveSeenLocation[key] = message;
-          all.push(message);
-        }
-        return all;
-      }, []);
-    });
-}
 
 export function getLocation() {
   return new Promise((resolve) => {
@@ -27,7 +8,7 @@ export function getLocation() {
         lng: position.coords.longitude
       });
     }, () => {      
-      resolve(fetch('https://ipapi.co/json')
+      resolve(fetch('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCPzxx1Hx18ZT4q2ONjkyFWYRVhlmNrN-I')
         .then(res => res.json())
         .then(location => {
           return {
@@ -37,14 +18,4 @@ export function getLocation() {
         }));
     });
   });
-}
-
-export function sendMessage(message) {
-  return fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(message)
-  }).then(res => res.json());
 }
